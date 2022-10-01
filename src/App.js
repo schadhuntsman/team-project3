3574713564b3bf349cc059d7cebe41958a63f15c
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-09-30 20:50:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import hdtv from './hdtv.png';
+import tv from './pngwing.com.png';
+import char from './paperChar.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    const [charLocation, setCharLocation] = useState({ top: 500, left: 700 });
+
+    const handleCharMove = async (top, left) => {
+        const animationNum = 5;
+        const distanceToTop = top - charLocation.top;
+        const distanceToLeft = left - charLocation.left;
+        const travelTop = Math.round(distanceToTop / animationNum);
+        const travelLeft = Math.round(distanceToLeft / animationNum);
+
+        let updatedState;
+
+        for(let i = 0; i <= animationNum; i++) {
+            updatedState = {...charLocation, top: (charLocation.top + travelTop), left: (charLocation.left + travelLeft)}
+            setCharLocation({...charLocation, ...updatedState});
+        }
+
+    }
+    return (
+        <div onClick={(e) => handleCharMove(e.pageY, e.pageX)} style={{ height: '2000px', width: '2000px' }}>
+            <InteractObject image={tv} glowyImage={hdtv} location={{ top: '500px', left: '500px' }} />
+            <Character location={charLocation} />
+        </div>
+    )
 }
 
-export default App;
+const InteractObject = ({ image, glowyImage, location }) => {
+    const [isHovered, setHovered] = useState(false);
+
+    return (
+        <div>
+            <img onMouseLeave={() => setHovered(false)}
+                onMouseOver={() => setHovered(true)}
+                alt="interact" src={isHovered ? glowyImage : image}
+                style={{ border: isHovered ? '2px solid gold' : '', height: '50px', width: '50px', position: 'absolute', top: location.top, left: location.left }}>
+            </img>
+        </div>
+    )
+}
+
+const Character = ({ location }) => {
+
+    return (
+        <div>
+            <img alt="char" style={{ position: 'absolute', height: '100px', width: '50px', top: `${location.top}px`, left: `${location.left}px` }} src={char} />
+        </div>
+    )
+}
